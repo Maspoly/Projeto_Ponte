4949abb71ba7206dccf3c8ceda4a13f0297f0787
Maspoly <maspolyfilho7@gmail.com>
2025-11-28 16:13:44 -0300

    Refactor line direction cosine calculations and matrix creation

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ p1 = Point(1, 2, 3)
 p2 = Point(4, 5, 6)
 line = Line(p1, p2)
 
-print(f"Line Length: {line.length()}")
-print(f"Direction Cosine X: {line.direction_cosine_x()}")
-print(f"Direction Cosine Y: {line.direction_cosine_y()}")
-print(f"Direction Cosine Z: {line.direction_cosine_z()}")
+l = line.direction_cosine_x()
+m = line.direction_cosine_y()
+n = line.direction_cosine_z()
+
+
+matrix = [[l**2, l * m, l * n, -(l**2), -(l * m), -(l * n),],
+          [l * m, m**2, m * n, -(l * m), -(m**2), -(m * n),],
+          [l * n, m * n, n**2, -(l * n), -(m * n), -(n**2),],
+          [-(l**2), -(l * m), -(l * n), l**2, l * m, l * n,],
+          [-(l * m), -(m**2), -(m * n), l * m, m**2, m * n,],
+          [-(l * n), -(m * n), -(n**2), l * n, m * n, n**2,]]
 
 
